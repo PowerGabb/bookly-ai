4a6caa5cdfc9e2265e2f41bd37f2fa56e6cae426
Rangga Bayu Pratama <110972066+PowerGabb@users.noreply.github.com>
2025-02-09 10:55:26 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get('/', (req, res) => {
-    res.send('Welcome To Bookly API');
+    res.send('Welcome To Bookly API Developers');
 });
 app.use("/api", router);
 
