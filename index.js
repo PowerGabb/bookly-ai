e30163ddd07b64dce3ca7cfdf291ae447147b199
Rangga Bayu Pratama <110972066+PowerGabb@users.noreply.github.com>
2025-02-09 11:09:20 +0700

    pindahkan index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/index.js";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import router from "./src/routes/index.js";
 dotenv.config();
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
